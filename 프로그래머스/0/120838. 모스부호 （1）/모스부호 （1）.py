0c650f56e9d843fce6019ac8baeddd61dc071997
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-03-03 21:55:10 +0900

    feat: 프로그래머스 lv0 120838. 모스부호 （1） 풀이 추가

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -0,0 +1,15 @@
+def solution(letter):
+    answer = ''
+    morse = { 
+        '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+        '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+        '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+        '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+        '-.--':'y','--..':'z'
+    }
+    letters = list(letter.split())
+    for letter in letters:
+        if letter in morse.keys():
+            answer += morse[letter]
+            
+    return answer
