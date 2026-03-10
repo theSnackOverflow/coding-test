e3d9c400c111b94a5ec44b2d2511430bdae1ea56
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-03-10 15:01:30 +0900

    feat: 프로그래머스 lv0 181926. 수 조작하기 1 풀이 추가

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -0,0 +1,12 @@
+def solution(n, control):
+    for char in control:
+        if char == "w":
+            n += 1
+        elif char == "s":
+            n -= 1
+        elif char == "d":
+            n += 10
+        elif char == "a":
+            n -= 10
+        else: pass
+    return n
