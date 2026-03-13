e4a11f02524aca66b9572cbf6cf6f384ac7c68ff
Issac <163278029+theSnackOverflow@users.noreply.github.com>
2026-03-14 01:18:00 +0900

    [Bronze V] Title: 개수 세기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -5,4 +5,9 @@ n = int(input())
 num_arr = list(map(int, input().split()))
 v = int(input())
 
-print(num_arr.count(v))
+count = 0
+for num in num_arr:
+    if num == v:
+        count += 1
+
+print(count)
